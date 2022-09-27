1182da7ad8dd1be7e82aa1df9c16a103d8556640
moonbeamglitterblossom <67506475+moonbeamglitterblossom@users.noreply.github.com>
2022-09-27 16:40:03 +0200

    Add test lib

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AppComponent, children: [
+      {path: '', loadChildren: () => import('@testlib/core').then(m => m.TestLibModule)}]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
